abb3662d9f8847f462e09347a345a2038ed8a281
SkyFrost <skyfrost.07@gmail.com>
2018-10-16 22:44:22 +0700

    unsubscribe

--- modules/front/resources/views/layouts/mail.blade.php
+++ modules/front/resources/views/layouts/mail.blade.php
@@ -18,7 +18,7 @@ and open the template in the editor.
         <hr />
         
         <p style="font-size: 13px;">
-            Thực sự không muốn như thế này đâu, nhưng nễu lỡ không thích rồi thì đừng vui lòng mà hãy một chút do dự bấn nút này nhé! <a href="{{ $unsubsLink }}">Hủy đăng ký</a>
+            Thực sự không muốn như thế này đâu, nhưng nễu lỡ không thích rồi thì đừng vui lòng mà hãy một chút do dự bấm nút này nhé! <a href="{{ $unsubsLink }}">Hủy đăng ký</a>
         </p>
         @endif
     </body>
